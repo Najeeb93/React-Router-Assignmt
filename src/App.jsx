0208922b495fa23171bdf4ab4f12fa52950297d3
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-07 12:42:49 +0500

    add to changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1 className='bg-slate-700 text-blue-900'>Hello World</h1>
+      <BrowserRouter>
+      </BrowserRouter>
     </>
   )
 }
