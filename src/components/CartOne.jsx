97ddbb375ba16169e17f86e77f18e9abbc65a742
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-09-12 19:57:43 +0500

    upload

--- src/components/CartOne.jsx
+++ src/components/CartOne.jsx
@@ -96,12 +96,7 @@ export function CartOne() {
         >
           Back to Home
         </button></Link>
-        <button
-          type="button"
-          className="rounded-md border border-black px-3 py-2 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-        >
-          Checkout
-        </button>
+      
       </div>
     </div>
   )
